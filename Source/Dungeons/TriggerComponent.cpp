54d952901f02501774bc825eca04bc2b34e3c8e3
Szwagrowicz <mateusz.prystupa@gmail.com>
2024-08-20 21:54:51 +0200

    add TriggerComponent

--- Source/Dungeons/TriggerComponent.cpp
+++ Source/Dungeons/TriggerComponent.cpp
@@ -3,9 +3,42 @@
 
 #include "TriggerComponent.h"
 
+UTriggerComponent::UTriggerComponent()
+{
+    PrimaryComponentTick.bCanEverTick = true;
+}
+
 void UTriggerComponent::BeginPlay()
 {
 	Super::BeginPlay();
-
-    UE_LOG(LogTemp, Display, TEXT("Trigger component alive"));
 }
+
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    AActor* Actor = GetAcceptableActor();
+    if(Actor != nullptr)
+    {
+        UE_LOG(LogTemp, Display, TEXT("Unlocking"));
+    }
+    else
+    {
+        UE_LOG(LogTemp, Display, TEXT("Relocking"));
+    }
+}
+
+AActor* UTriggerComponent::GetAcceptableActor() const
+{
+    TArray<AActor*> Actors;
+    GetOverlappingActors(Actors);
+    for (AActor* Actor : Actors)
+    {
+        if(Actor->ActorHasTag(AcceptableActorTag))
+        {
+            return Actor;
+        }
+    }
+    return nullptr;
+}
+
--- Source/Dungeons/TriggerComponent.h
+++ Source/Dungeons/TriggerComponent.h
@@ -9,12 +9,27 @@
 /**
  * 
  */
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class DUNGEONS_API UTriggerComponent : public UBoxComponent
 {
 	GENERATED_BODY()
 	
+
+public:
+	UTriggerComponent();
+
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
+
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+
+
+	UPROPERTY(EditAnywhere)
+	FName AcceptableActorTag;
+private:
+	AActor* GetAcceptableActor() const;
 };
